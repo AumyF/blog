3fb71121017b381f60f38d0e9249cad452a8f294
Aumy <aumy@outlook.jp>
2023-10-05 23:34:29 +0900

    Flake and bun

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "blog.fuku.day";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ bun act ];
+        };
+      }
+    );
+}
